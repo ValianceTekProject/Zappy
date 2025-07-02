c13130a48146da03b5eaffb3b93f90bb48319eb4
Oursegamin <axel.lavrador@gmail.com>
2025-07-02 17:12:45 +0200

    fix: players tests

--- GUI/src/Renderer/Raylib/RaylibRenderer.cpp
+++ GUI/src/Renderer/Raylib/RaylibRenderer.cpp
@@ -27,15 +27,6 @@ void zappy::gui::RaylibRenderer::init()
 
     this->_gameMenu = std::make_unique<raylib::GameMenu>(this->_gameState);
     this->_gameMenu->init();
-
-    for (int id = 0; id < 11; ++id) {
-        game::Player p(id, std::rand() % 10, std::rand() % 10);
-        p.teamName = "Team " + std::to_string(id % 2);
-        this->addPlayer(p);
-        game::Inventory inv;
-        inv.addResource(game::Resource::FOOD, 10);
-        this->updatePlayerInventory(id, inv);
-    }
 }
 
 void zappy::gui::RaylibRenderer::setFrequency(const size_t &frequency)
